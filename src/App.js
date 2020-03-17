4875db87bf94eac5a5ca1e33ff5612d877f6cf5d
Michel Foucault <michel@condensat.tech>
2020-03-17 20:00:00 +0000

    Call session Renew and Close
    - add separate sessionState function
    - use 2 useEffect:
      initial authenticated state and redirect user to login page
      renew session while logged in (30sec)
    - remove isAuthenticating

--- src/App.js
+++ src/App.js
@@ -2,36 +2,36 @@ import React, { useState, useEffect } from 'react';
 import { Link, withRouter } from "react-router-dom";
 import { Nav, Navbar, NavItem } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
+import { useCookies } from 'react-cookie';
 
 import Routes from "./Routes";
 
 import HomeIcon from '@material-ui/icons/HomeOutlined';
 import LogoutIcon from '@material-ui/icons/ExitToAppOutlined';
 import LoginIcon from '@material-ui/icons/FingerprintOutlined';
+
+import * as bank_api from "/js/bank-api.min.js";
+
 import "./App.css";
 
+// sessionState renew session with timer
+var sessionState = function(props) { }
 
 function App(props) {
-  const [isAuthenticating, setIsAuthenticating] = useState(true);
   const [isAuthenticated, userHasAuthenticated] = useState(false);
 
-  function onLoad() {
-    // Todo - call bank api to check sessionId  
-    setIsAuthenticating(false);
-  }
+  props.isAuthenticated = isAuthenticated;
+  props.userHasAuthenticated = userHasAuthenticated;
+  sessionState(props)
 
-  useEffect(() => {
-    onLoad();
-  }, []);
-  
-  
-  function handleLogout() {
+  // close session
+  function closeSession() {
     userHasAuthenticated(false);
+    bank_api.closeSession(() => { });
     props.history.push("/login");
   }
 
   return (
-    !isAuthenticating &&
     <div className="App container">
       <Navbar bg="light" collapseOnSelect="true">
         <Navbar.Brand>
@@ -43,7 +43,7 @@ function App(props) {
         <Navbar.Collapse>
           <Nav className="ml-auto">
             {isAuthenticated
-              ? <NavItem onClick={handleLogout}>
+              ? <NavItem onClick={closeSession}>
                   <LogoutIcon className="Icon Link"></LogoutIcon>
                 </NavItem>
               : <>
@@ -61,6 +61,39 @@ function App(props) {
       <Routes appProps={{ isAuthenticated, userHasAuthenticated }} />
     </div>
   );
+}
+
+sessionState = (props) => {
+  const [cookies] = useCookies(['account']);
+
+  const { isAuthenticated, userHasAuthenticated } = props;
+  const [renewCount, setRenewCount] = useState(0);
+
+  // call once, setup initial isAuthenticated state
+  useEffect(() => {
+    bank_api.renewSession((err) => {
+      const incr = (err != null) ? 0 : 1;
+      userHasAuthenticated(incr > 0);
+
+      // if session is not valid, and account found in cookie, go to login page
+      if (incr == 0 && cookies.account) {
+        props.history.push("/login");
+      }
+    })
+  }, []);
+
+  // renew session timer
+  useEffect(() => {
+      // skip if not authenticated
+      if (!isAuthenticated) {
+        return;
+      }
+    setTimeout(bank_api.renewSession, 30000, (err) => {
+      const incr = (err != null) ? 0 : 1;
+      userHasAuthenticated(incr > 0);
+      setRenewCount(renewCount + incr);
+    });
+  }, [isAuthenticated, renewCount]);
 }
 
 export default withRouter(App);
