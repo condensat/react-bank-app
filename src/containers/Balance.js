5b0b7e059e33c4fe4f08ff27e3197af5604b9549
Michel Foucault <michel@condensat.tech>
2020-03-18 08:00:00 +0000

    Add routes and containers
    - Balance
    - Receive
    - Send
    - Settings

---
+++ src/containers/Balance.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "./Balance.css";
+
+const Balance = () => {
+
+  return (
+    <div className="Balance">
+      <div className="lander">
+        <h1>Balance</h1>
+        <p>Currencies</p>
+        <p>Transactions</p>
+      </div>
+    </div>
+  );
+}
+
+export default Balance;
---
+++ src/containers/Receive.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./Receive.css";
+
+const Receive = () => {
+
+  return (
+    <div className="Receive">
+      <div className="lander">
+        <h1>Receive funds</h1>
+        <p>Account</p>
+      </div>
+    </div>
+  );
+}
+
+export default Receive;
