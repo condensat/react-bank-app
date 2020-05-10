7d094cf499b41011bd9a9b93343c8a9c991fd189
Michel Foucault <michel@condensat.tech>
2020-05-10 19:00:00 +0000

    Update Balance view
    - add deposit qr link
    - remove some columns headers
    - fix login redirect on refresh

--- src/containers/Balance.js
+++ src/containers/Balance.js
@@ -3,13 +3,31 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
 import AccountBalanceWalletIcon from '@material-ui/icons/AccountBalanceWallet';
+import DepositIcon from '@material-ui/icons/CenterFocusWeak';
 import HistoryIcon from '@material-ui/icons/History';
+import LoginIcon from '@material-ui/icons/FingerprintOutlined';
 
 import "./Balance.css";
 
 import * as bank_api from "/js/bank-api.min.js";
 
 import BootstrapTable from 'react-bootstrap-table-next';
+
+const CellAssetIcon = (base64) => {
+  if (base64) {
+    return (
+      <img className="TickerIcon" src={'data:image/png;base64,'+base64} />
+    )
+  }
+  return ""
+}
+
+const CellNotionalBalance = (balance) => {
+  if (balance) {
+    return balance
+  }
+  return ""
+}
 
 const CellHistory = (cell, row) => {
   return (
@@ -22,35 +40,54 @@ const CellHistory = (cell, row) => {
   )
 }
 
+const CellDeposit = (cell, row) => {
+  const isCrypto = row.isCrypto;
+
+  return (
+    <>
+    {isCrypto
+      ? 
+      <Link to={{
+        pathname: '/qr',
+        state: row
+      }}>
+        <DepositIcon className="IconSmall" />
+      </Link>
+      : <></>
+    }
+    </>
+  )
+}
+
 const Balance = (props) => {
   const isAuthenticated = props.isAuthenticated
   const [accounts, setAccounts] = useState([]);
 
   const columns = [{
-    dataField: 'id',
-    text: 'Account ID',
+    dataField: 'icon',
+    formatter: CellAssetIcon
   }, {
-    dataField: 'notionalCurrency',
-    text: 'Notional Currency'
+    dataField: 'displayName',
+    text: 'Name'
+  }, {
+    dataField: 'notionalTicker',
+    text: 'Notional Ticker'
   }, {
     dataField: 'notionalBalance',
-    text: 'Notional Balance'
+    text: 'Notional Balance',
+    formatter: CellNotionalBalance
   }, {
-    dataField: 'status',
-    text: 'Account Status'
-  }, {
-    dataField: 'currency',
-    text: 'Currency'
+    dataField: 'ticker',
+    text: 'Ticker'
   }, {
     dataField: 'balance',
     text: 'Balance'
   }, {
-    dataField: 'totalLocked',
-    text: 'Locked'
-  }, {
     dataField: 'history',
-    text: 'History',
     formatter: CellHistory
+  }, {
+    dataField: 'deposit',
+    formatter: CellDeposit
   }];
 
   const defaultSorted = [{
@@ -59,13 +96,7 @@ const Balance = (props) => {
   }];
 
   useEffect(() => {
-    if (!isAuthenticated) {
-      props.history.push("/login");
-    }
-  })
-
-  useEffect(() => {
-    if (accounts.length == 0) {   
+    if (accounts.length == 0) {
       // fetch accounts info
       bank_api.accountList({ rateBase: "CHF" }, (err, result) => {
         if (err) {
@@ -75,18 +106,54 @@ const Balance = (props) => {
 
         // construct table data source
         var id = 0;
-        var entries = [];
+        var entriesAsset = [];
+        var entriesAssetWithoutIcon = [];
+        var entriesCrypto = [];
+        var entriesFiat = [];
+        var entriesOther = [];
         result.accounts.forEach(account => {
           // flatten nested data
           account["id"] = id++;
           account["history"] = account.accountId;
-          account["currency"] = account.curency.name;
-          account["notionalCurrency"] = account.notional.rateBase;
-          account["notionalBalance"] = account.notional.balance;
-          entries.push(account);
+          account["ticker"] = account.curency.ticker;
+          account["isCrypto"] = account.curency.isCrypto;
+          account["displayName"] = account.curency.displayName;
+          if (!account.curency.displayName) {
+            account["displayName"] = "No Name"
+          }
+          account["icon"] = account.curency.icon
+          account["notionalTicker"] = ""
+          account["notionalBalance"] = null
+          if (account.notional) {
+            account["notionalTicker"] = account.notional.rateBase;
+            account["notionalBalance"] = account.notional.balance;
+          }
+
+          if (account.curency.isAsset && account.curency.ticker != "TBTC") {
+            if (account.curency.displayName || account.curency.icon) {
+              if (account.curency.icon) {
+                entriesAsset.push(account);
+              } else {
+                entriesAssetWithoutIcon.push(account);
+              }
+            } else {
+              entriesOther.push(account);
+            }
+          } else if (account.curency.isCrypto) {
+            entriesCrypto.push(account);
+          } else {
+            entriesFiat.push(account);
+          }
         });
+
+        var entries = [];
+        entries = entries.concat(entriesFiat);
+        entries = entries.concat(entriesCrypto);
+        entries = entries.concat(entriesAsset);
+        entries = entries.concat(entriesAssetWithoutIcon);
+        entries = entries.concat(entriesOther);
         
-        setAccounts(entries)
+        setAccounts(entries);
       });
     }
   }, [accounts]);
@@ -96,7 +163,7 @@ const Balance = (props) => {
       <div className="lander">
       {isAuthenticated
         ? <>
-            <h1><AccountBalanceWalletIcon className="Icon" />Balances</h1>
+            <h1><AccountBalanceWalletIcon className="Icon" />Accounts</h1>
             <BootstrapTable
               bootstrap4
               keyField="id"
@@ -105,7 +172,13 @@ const Balance = (props) => {
               defaultSorted={defaultSorted}
             />
           </>
-        : <></>
+        : <>
+            <div className="Login">
+              <Link to="/login">
+                <h2><LoginIcon className="Icon Link" />Login</h2>
+              </Link>
+            </div>
+          </>
       }
       </div>
     </div>
